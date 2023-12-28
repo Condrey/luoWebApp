edd516cca24a1d15c5eeb813d2ebc706d3a74342
CondreyJames <wangzdrey@gmail.com>
2023-12-29 01:27:33 +0300

    2023-12-29 1:27AM

--- src/app/(homeComponents)/Logo.tsx
+++ src/app/(homeComponents)/Logo.tsx
@@ -3,11 +3,12 @@ import Image from "next/image";
 import logo from "@/assets/Transparent Backgrounds (1).png";
 import {webPageName} from "@/lib/constants/Constants";
 import {lusitana} from "@/app/ui/fonts";
+import {cn} from "@/lib/utils";
 
 export default function Logo() {
     return <div>
-        <Link href="/" className={` ${lusitana.className} items-center gap-1 `}>
-            <Image src={logo} alt={"Luo.com logo"} width={60} height={60} className=' '/>
+        <Link href="/" className={cn(lusitana.className, `flex  items-center gap-1 `)}>
+            <Image src={logo} alt={"Luo.com logo"} width={40} height={40} className=' '/>
             <span className='font-bold text-slate-700 uppercase'>{webPageName}</span>
         </Link>
     </div>
