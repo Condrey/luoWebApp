4474aa1ab6ee253de8a7549d6d1d05cf3dc41274
CondreyJames <wangzdrey@gmail.com>
2024-01-11 17:45:05 +0300

    2024-01-11 5:44PM

--- src/app/(homeNavBarPages)/grievances/layout.tsx
+++ src/app/(homeNavBarPages)/grievances/layout.tsx
@@ -1,13 +1,14 @@
 import SideNav from "@/app/(homeNavBarPages)/grievances/(grievancesComponents)/SideNav";
 
-export default function Layout({children}: { children: React.ReactNode }) {
-    return (
-        <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
-            <div className="sticky top-0 bg-background/50 z-50 backdrop-blur-2xl w-full flex-none md:w-64 ">
-                <SideNav/>
-            </div>
-            <div
-                className="flex-grow   md:overflow-y-auto  bg-accent dark:bg-background   overscroll-y-none">{children}</div>
-        </div>
-    );
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
+      <div className="sticky top-0 bg-background/50 z-50 backdrop-blur-2xl w-full flex-none md:w-64 dark:md:border-r ">
+        <SideNav />
+      </div>
+      <div className="flex-grow    md:overflow-y-auto  bg-accent dark:bg-background   overscroll-y-none">
+        {children}
+      </div>
+    </div>
+  );
 }
