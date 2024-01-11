7f4cd8847864b074694dcd8e484f9c2a80d3b6a4
CondreyJames <wangzdrey@gmail.com>
2024-01-11 17:19:45 +0300

    2024-01-11 5:19PM

--- src/lib/db/data/donate-reminder-data.ts
+++ src/lib/db/data/donate-reminder-data.ts
@@ -6,7 +6,7 @@ export async function fetchHasUserSentDonateReminder() {
   noStore();
   try {
     const { userId } = await auth();
-    const result = await prisma.donateReminder.count({ where: { userId } });
+    const result = await prisma.donateReminder.count({ where: { userId:userId! as string } });
     return result > 0;
   } catch (e) {
     console.error("Error occurred:", e);
